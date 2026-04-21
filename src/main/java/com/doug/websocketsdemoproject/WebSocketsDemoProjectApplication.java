dc323261aa03178452f97050878c572536d10096
douglanyabasa <douglasnyabasa400@gmail.com>
2026-04-21 21:27:16 +0200

    first commit

---
+++ src/main/java/com/doug/websocketsdemoproject/WebSocketsDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.doug.websocketsdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketsDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketsDemoProjectApplication.class, args);
+    }
+
+}
